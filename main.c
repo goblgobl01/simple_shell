3d49064049bb622b02862ef949ba9b93df97d473
Mohammed Maarafi <maarafimohammed@gmial.com>
2023-11-18 01:15:32 +0100

    alo

---
+++ main.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/**
+ * main - Entry point of the program
+ *
+ * Description: The main function prints "hello world!" to the console.
+ * It uses the printf function to display the message.
+ *
+ * Return: Always 0 (indicating success)
+ */
+int main(void)
+{
+	printf("hello world!\n");
+	return (0);
+}
