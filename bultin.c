0585d05817c7c03fee50c708afacda9099cd56d4
Mohammed Maarafi <maarafimohammed@gmial.com>
2023-11-18 01:35:42 +0100

    alo

--- bultin.c
+++ bultin.c
@@ -7,26 +7,24 @@
  */
 int execute_builtin(char **arguments, char **envp)
 {
-if (arguments[0] == NULL)
-{
-return (1);
-}
-else if (my_strcmp(arguments[0], "exit") == 0)
-{
-free_tokens(arguments);
-exit(0);
-}
-else if (my_strcmp(arguments[0], "env") == 0)
-{
-char **env = envp;
-while (*env != NULL)
-{
-printf("%s\n", *env);
-env++;
-}
-free_tokens(arguments);
-return (1);
-}
+	if (arguments[0] == NULL)
+		return (1);
+	else if (my_strcmp(arguments[0], "exit") == 0)
+	{
+		free_tokens(arguments);
+		exit(0);
+	}
+	else if (my_strcmp(arguments[0], "env") == 0)
+	{
+		char **env = envp;
+	while (*env != NULL)
+	{
+		printf("%s\n", *env);
+		env++;
+	}
+		free_tokens(arguments);
+		return (1);
+	}
 
-return (0);
+	return (0);
 }
